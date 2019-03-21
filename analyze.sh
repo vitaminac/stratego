5fd44e0a5220f4a9056bfd295ca18e99b74c2e95
daolao <dalao1002@gmail.com>
2019-03-21 14:24:48 +0100

    sonar

--- analyze.sh
+++ analyze.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-./gradlew sonarqube \
-  -Dsonar.projectKey=vitaminac_stratego \
-  -Dsonar.organization=vitaminac-github \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=89be7544bf7650efa0e33509b7bd2648fd69b59d
+.\gradlew sonarqube -Dsonar.projectKey=vitaminac_stratego -Dsonar.organization=vitaminac-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=89be7544bf7650efa0e33509b7bd2648fd69b59d
