8dbca2315d098bae3c486fbb486665de6e7a79a3
Santi <santi.cervantes@hotmail.es>
2019-04-04 11:45:17 +0200

    Player class Gradle issues fixed

--- src/edu/asu/stratego/game/Player.java
+++ src/edu/asu/stratego/game/Player.java
@@ -2,40 +2,37 @@ package edu.asu.stratego.game;
 
 import java.io.Serializable;
 
-<<<<<<< HEAD:src/edu/asu/stratego/game/Player.java
-=======
 
->>>>>>> master:src/main/java/edu/asu/stratego/game/Player.java
 /**
  * Contains information about a player.
  */
 public class Player implements Serializable {
-    
+
     private static final long serialVersionUID = 649459794036226272L;
     private String nickname;
     private PieceColor color;
-    
+
     /**
      * @param nickname the player's nickname
      */
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
-    
+
     /**
      * @return a String containing the player's name
      */
     public String getNickname() {
         return nickname;
     }
-    
+
     /**
      * @param color the player's color
      */
     public void setColor(PieceColor color) {
         this.color = color;
     }
-    
+
     /**
      * @return the player's color
      */
